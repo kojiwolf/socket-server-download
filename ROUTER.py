4e1fbc19aa7485432e98d0cf3de9d7b8f4a79a57
iloveeggsandwiches <149441679+iloveeggsandwiches@users.noreply.github.com>
2024-06-06 13:04:14 -0600

    Update ROUTER.py

--- ROUTER.py
+++ ROUTER.py
@@ -5,16 +5,28 @@ ServerData = ["scratchcat", "apple", "kojiwolf", "koji", "loki", "lemon", "lemon
 
 def socket_request(*args):
     global ServerResponseArray
-    x = random.randint(100000, 999999)
+    x = random.randint(100000000000000000000000000000000000000000000000, 999999999999999999999999999999999999999999999999)
     ServerResponseArray = []
     
     for index in args:
-        if isinstance(index, int) and 0 <= index < len(ServerData):
-            ServerResponseArray.append(ServerData[index])
+        try:
+            if isinstance(index, int) and 0 <= index < len(ServerData):
+                ServerResponseArray.append(ServerData[index])
+            else:
+                raise ValueError(f"Index {index} is out of range or not an integer.")
+        except ValueError as e:
+            print(f"Error: {e}")
     
     resp = ",".join(ServerResponseArray)
     fstring = f"{{({resp}),(SOCKET:1,SOCKET-REQUEST-ID:0x{x})}}"
     return fstring
-def help():
-    global ServerData
-    print(ServerData)
+
+
+def socket_index(input):
+    x = random.randint(100000000000000000000000000000000000000000000000, 999999999999999999999999999999999999999999999999)
+    try:
+        resp = ServerData.index(input)
+        fstring = f"{{{{{resp}}},(SOCKET:1,SOCKET-REQUEST-ID:0x{x})}}"
+        return fstring
+    except ValueError:
+        return f"Error: '{input}' is not in ServerData."
