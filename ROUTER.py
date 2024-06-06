ce668f1bc6aa0a2559f8e5fb8365d070a1146073
kojiwolf <kojiwolfscratch@gmail.com>
2024-06-06 12:58:45 -0600

    koji

--- ROUTER.py
+++ ROUTER.py
@@ -5,7 +5,7 @@ ServerData = ["scratchcat", "apple", "kojiwolf", "koji", "loki", "lemon", "lemon
 
 def socket_request(*args):
     global ServerResponseArray
-    x = random.randint(100000, 999999)
+    x = random.randint(100000000000000000000000000000000000000000000000, 999999999999999999999999999999999999999999999999)
     ServerResponseArray = []
     
     for index in args:
@@ -17,4 +17,8 @@ def socket_request(*args):
     return fstring
 
 
-
+def socket_index(input):
+    x = random.randint(100000000000000000000000000000000000000000000000, 999999999999999999999999999999999999999999999999)
+    resp = ServerData.index(input)
+    fstring = f"{{{{{resp}}},(SOCKET:1,SOCKET-REQUEST-ID:0x{x})}}"
+    return fstring 
