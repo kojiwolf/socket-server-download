2349084b8d83de1a361e004adb901f1493ea5018
iloveeggsandwiches <149441679+iloveeggsandwiches@users.noreply.github.com>
2024-06-06 12:17:20 -0600

    Update ROUTER.py

--- ROUTER.py
+++ ROUTER.py
@@ -15,6 +15,6 @@ def socket_request(*args):
     resp = ",".join(ServerResponseArray)
     fstring = f"{{({resp}),(SOCKET:1,SOCKET-REQUEST-ID:0x{x})}}"
     return fstring
-
-
-
+def help():
+    global ServerData
+    print(ServerData)
